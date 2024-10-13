afa758383089fc87ac59b546bf8e2cbe2126e884
daria_hk <honcharuk.daria95@gmail.com>
2024-10-13 14:10:06 +0200

    add filles

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -13,7 +13,7 @@ fetchUsersBtn.addEventListener("click", handleSearch);
 function handleSearch(evt) {
     evt.preventDefault();
     let searchInput = document.querySelector('input[name="search"]');
-    let notFoundTextEl = document.querySelector('not-found-el');
+    let notFoundTextEl = document.querySelector('.not-found-el');
     let searchValue = searchInput.value.trim();
     
 
@@ -32,6 +32,11 @@ function handleSearch(evt) {
       if (data.total === 0) {
         imgs.innerHTML = '';
         notFoundTextEl.innerHTML = `Results for query <span>${searchValue}</span> not found!`;
+        iziToast.show({
+            title: "Error",
+            message: "Sorry, there are no images matching your search query. Please try again!",
+            color: "red",
+        });
         return;
       }
 
